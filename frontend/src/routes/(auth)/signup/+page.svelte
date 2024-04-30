5512dc40821118da28de5419c53d0f6dda1ea0e6
idsmoura <cleophasbarwareng@ids.company>
2024-04-30 12:16:54 +0300

    feat: add signup page

---
+++ frontend/src/routes/(auth)/signup/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Label } from '$lib/components/ui/label';
+	import { Input } from '$lib/components/ui/input';
+	let email: string;
+	let password: string;
+	$: console.log(email, password);
+</script>
+
+<div class="mx-auto flex h-screen max-w-sm items-center justify-center">
+	<Card.Root>
+		<Card.Header class="space-y-1">
+			<Card.Title class="text-2xl">Create an account</Card.Title>
+			<Card.Description>Enter your email below to create your account</Card.Description>
+		</Card.Header>
+		<Card.Content class="grid gap-4">
+			<div class="grid gap-2">
+				<Label for="email">Email</Label>
+				<Input id="email" type="email" bind:value={email} placeholder="m@example.com" />
+			</div>
+			<div class="grid gap-2">
+				<Label for="password">Password</Label>
+				<Input id="password" type="password" bind:value={password} />
+			</div>
+		</Card.Content>
+		<Card.Footer class="flex flex-col">
+			<Button
+				disabled={!password || !email}
+				class="w-full"
+				on:click={() => {
+					// signupWithEmailAndPassword(email, password);
+				}}>Create account</Button
+			>
+			<Button variant="link" href="/signin">Or Sign In</Button>
+		</Card.Footer>
+	</Card.Root>
+</div>
